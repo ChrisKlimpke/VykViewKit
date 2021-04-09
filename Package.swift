f77dc75bd69e7e3bcb16ebd1c5a64ab9801a9e29
Chris <chrisk@cityc.co.uk>
2021-04-09 06:41:43 +0100

    Initial SPM release

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "VykViewKit",
+  
+    platforms: [
+        .iOS(.v11),
+        .macOS("999.0")
+      ],
+  
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,15 +20,12 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+      .package(url: "git@github.com:ChrisKlimpke/TrackerSDKv2.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "VykViewKit",
-            dependencies: []),
-        .testTarget(
-            name: "VykViewKitTests",
-            dependencies: ["VykViewKit"]),
+      .binaryTarget(name: "VykViewKit",
+                    path: "VykViewKit.xcframework"),
     ]
 )
