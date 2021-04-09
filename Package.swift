9ccde886fa0958fa9220ae738360f409540d5b6e
Chris <chrisk@cityc.co.uk>
2021-04-09 07:58:16 +0100

    Changed xib file

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .iOS(.v11),
         .macOS("999.0")
       ],
-  
+     
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
